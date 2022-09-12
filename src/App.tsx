9de699450b4aa84de1ab5ec0c115cf1a5a3134dd
johnascott <js@heyorca.com>
2022-09-12 19:18:19 -0300

    components path alias created, now can refer to components/* instead of ./src/components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import Home from "components/Home/Home"
 
 export default function App(){
     return(
         <View>
-            <Text>Hello</Text>
+            <Home></Home>
         </View>
     )
 }
